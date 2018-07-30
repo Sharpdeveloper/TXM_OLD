192c1303683404f4cd953edf6e134333dbfe6701
Sharpdeveloper <martin@Martins-Air.fritz.box>
2018-07-30 18:06:39 +0200

    Saved C# Version

---
+++ C#_OLD/src/TXM.Core/Setting.cs
@@ -0,0 +1,37 @@
+﻿namespace TXM.Core
+{
+    public sealed class Settings
+    {
+        #region Constants
+        private const string fileextension = "txmtournament";
+        private const string fileextensionName = "TXM Tournaments";
+        private const string txmversion = "V2.2.2";
+        #endregion
+
+        #region Properties
+        public static string FILEEXTENSION
+        {
+            get
+            {
+                return fileextension;
+            }
+        }
+
+        public static string TXMVERSION
+        {
+            get
+            {
+                return txmversion;
+            }
+        }
+
+        public static string FILEEXTENSIONSNAME
+        {
+            get
+            {
+                return fileextensionName;
+            }
+        }
+        #endregion
+    }
+}
